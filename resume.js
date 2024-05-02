3a3c76fca7252c8b90e2c31a919992d7055c132e
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-05-02 22:43:52 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,77 @@
+let res={
+"Name":"Salma M", 
+"label":"Developer",
+ "Email":"sal@gmail.com",
+"Phone":8760907651,
+"location":{
+"Address":"24,Sakthi Colony",
+"postal code":641606,
+"city":"Tiruppur",
+'Country Code':"INDIA",
+},
+"profile":{
+"network":"facebook",
+"username":"XY",
+"url":"https:/",
+},
+"Education" :{
+"Institution":"Bharathiar University",
+"College":"Maharani Arts and Science College",
+"Degree":"M.Sc",
+"Duration":"2012-2014",
+"score":82,
+},
+"skills":{
+"name":"Web Development",
+"Level":"Beginner",
+"Key Words":["HTML","VB","JS"],
+},
+"Languages":{
+"Known Languages":["Tamil","Hindi","English"],
+"Fluency":["Tamil","Hindi"],
+},
+"Interest":{
+"Name":"Watching Movies",
+"Key words":"Rom-Com Movies",
+},
+"Reference":{
+"Name":"",
+"Mobile Number":"675*******",
+}
+};
+console.log(res)
+OUTPUT:
+{
+  Name: 'Salma M',
+  label: 'Developer',
+  Email: 'sal@gmail.com',
+  Phone: 8760907651,
+  location: {
+    Address: '24,Sakthi Colony',
+    'postal code': 641606,
+    city: 'Tiruppur',
+    'Country Code': 'INDIA'
+  },
+  profile: { network: 'facebook', username: 'XY', url: 'https:/' },
+  Education: {
+    Institution: 'Bharathiar University',
+    College: 'Maharani Arts and Science College',
+    Degree: 'M.Sc',
+    Duration: '2012-2014',
+    score: 82
+  },
+  skills: {
+    name: 'Web Development',
+    Level: 'Beginner',
+    'Key Words': [ 'HTML', 'VB', 'JS' ]
+  },
+  Languages: {
+    'Known Languages': [ 'Tamil', 'Hindi', 'English' ],
+    Fluency: [ 'Tamil', 'Hindi' ]
+  },
+  Interest: { Name: 'Watching Movies', 'Key words': 'Rom-Com Movies' },
+  Reference: { Name: '', 'Mobile Number': '675*******' }
+}
+
+
+
