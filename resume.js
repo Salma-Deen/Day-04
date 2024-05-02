0544910399b1f614245addf6cae4c887494e3fd3
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-05-02 22:47:24 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,3 +1,4 @@
+// Create your own resume data in JSON format
 let res={
 "Name":"Salma M", 
 "label":"Developer",
@@ -73,5 +74,56 @@ OUTPUT:
   Reference: { Name: '', 'Mobile Number': '675*******' }
 }
 
+2)For the above JSON,iterate over all for loops(for,for in for of,forEach)
+A)for loop:
+let res={
+        "Name":"Salma M", 
+   "label":"Developer",
+        "Email":"sal@gmail.com",
+   "Phone":8760907651,
+   "location":{
+    "Address":"24,Sakthi Colony",
+    "postal code":641606,
+    "city":"Tiruppur",
+    'Country Code':"INDIA",
+   },
+   "profile":{
+    "network":"facebook",
+    "username":"XY",
+    "url":"https:/",
+   },
+  "Education" :{
+"Institution":"Bharathiar University",
+"College":"Maharani Arts and Science College",
+"Degree":"M.Sc",
+"Duration":"2012-2014",
+"score":82,
+  },
+  "skills":{
+    "name":"Web Development",
+    "Level":"Beginner",
+    "Key Words":["HTML","VB","JS"],
+  },
+  "Languages":{
+    "Known Languages":["Tamil","Hindi","English"],
+    "Fluency":["Tamil","Hindi"],
+  },
+  "Interest":{
+"Name":"Watching Movies",
+"Key words":"Rom-Com Movies",
+  },
+  "Reference":{
+    "Name":"",
+    "Mobile Number":"675*******",
+  }
+};
+var resume=Object.keys(res);
+for(var i=0;i<resume.length;i++)
+{
+  console.log(res[resume[i]])
+}
+
+
+b)for in loop
 
 
