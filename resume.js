ef6dc850f1ba4599a0a31dd52fde4b24988b47f4
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-05-03 16:05:01 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -41,7 +41,9 @@ let res={
 }
 }
 console.log(res)
-OUTPUT:
+
+// OUTPUT:
+
 {
   Name: 'Salma M',
   label: 'Developer',
@@ -122,7 +124,9 @@ for(var i=0;i<resume.length;i++)
 {
   console.log(res[resume[i]])
 }
+
 // OUTPUT:
+
 Salma M
 Developer
 sal@gmail.com
@@ -200,7 +204,9 @@ for(var i in res)
 {
   console.log(res[i])
 }
-OUTPUT:
+
+// OUTPUT:
+
 Salma M
 Developer
 sal@gmail.com
