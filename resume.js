d13d0ffc4c445677fb980d5b3577a1e0d62bb19a
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-05-03 15:27:58 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -203,3 +203,95 @@ sal@gmail.com
 { Name: '', 'Mobile Number': '675*******' }
 
 
+C) for of loop:
+
+let resume={
+        "Name":"Salma M", 
+   "label":"Developer",
+        "Email":"sal@gmail.com",
+   "Phone":8760907651,
+   "location":{
+    "Address":"24,Sakthi Colony",
+    "postal code":641606,
+    "city":"Tiruppur",
+    'Country Code':"INDIA",
+   },
+   "profile":{
+    "network":"facebook",
+    "username":"XY",
+    "url":"https:/",
+   },
+  "Education" :{
+"Institution":"Bharathiar University",
+"College":"Maharani Arts and Science College",
+"Degree":"M.Sc",
+"Duration":"2012-2014",
+"score":82,
+  },
+  "skills":{
+    "name":"Web Development",
+    "Level":"Beginner",
+    "Key Words":["HTML","VB","JS"],
+  },
+  "Languages":{
+    "Known Languages":["Tamil","Hindi","English"],
+    "Fluency":["Tamil","Hindi"],
+  },
+  "Interest":{
+"Name":"Watching Movies",
+"Key words":"Rom-Com Movies",
+  },
+  "Reference":{
+    "Name":"",
+    "Mobile Number":"675*******",
+  }
+}
+for(const[key,value] of Object.entries(resume))
+{
+  if(typeof value==='object')
+  {
+  console.log(`${key}:`);
+  for(const[subkey,subvalue] of Object.entries(value))
+  {
+    console.log(`${subkey}: ${subvalue}`);
+  }
+}
+else{
+  console.log(`${key}: ${value}`);
+}
+}
+
+// OUTPUT:
+
+Name: Salma M
+label: Developer
+Email: sal@gmail.com
+Phone: 8760907651
+location:
+Address: 24,Sakthi Colony
+postal code: 641606
+city: Tiruppur
+Country Code: INDIA
+profile:
+network: facebook
+username: XY
+url: https:/
+Education:
+Institution: Bharathiar University
+College: Maharani Arts and Science College
+Degree: M.Sc
+Duration: 2012-2014
+score: 82
+skills:
+name: Web Development
+Level: Beginner
+Key Words: HTML,VB,JS
+Languages:
+Known Languages: Tamil,Hindi,English
+Fluency: Tamil,Hindi
+Interest:
+Name: Watching Movies
+Key words: Rom-Com Movies
+Reference:
+Name:
+Mobile Number: 675*******
