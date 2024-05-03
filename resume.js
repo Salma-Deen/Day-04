16420f7cc5f62d9755495d65bd1d7c7cb1fa8abd
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-05-03 15:32:59 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -122,7 +122,36 @@ for(var i=0;i<resume.length;i++)
 {
   console.log(res[resume[i]])
 }
-
+// OUTPUT:
+Salma M
+Developer
+sal@gmail.com
+8760907651
+{
+  Address: '24,Sakthi Colony',
+  'postal code': 641606,
+  city: 'Tiruppur',
+  'Country Code': 'INDIA'
+}
+{ network: 'facebook', username: 'XY', url: 'https:/' }
+{
+  Institution: 'Bharathiar University',
+  College: 'Maharani Arts and Science College',
+  Degree: 'M.Sc',
+  Duration: '2012-2014',
+  score: 82
+}
+{
+  name: 'Web Development',
+  Level: 'Beginner',
+  'Key Words': [ 'HTML', 'VB', 'JS' ]
+}
+{
+  'Known Languages': [ 'Tamil', 'Hindi', 'English' ],
+  Fluency: [ 'Tamil', 'Hindi' ]
+}
+{ Name: 'Watching Movies', 'Key words': 'Rom-Com Movies' }
+{ Name: '', 'Mobile Number': '675*******' }
 
 b)for in loop
 let res={
@@ -295,3 +324,5 @@ Key words: Rom-Com Movies
 Reference:
 Name:
 Mobile Number: 675*******
+
+ D)forEach: 
